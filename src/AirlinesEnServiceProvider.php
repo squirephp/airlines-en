7fa914e71205c3fda50f141523751a68f0f89af1
Dan Harrin <dan@danharrin.com>
2021-01-11 08:50:45 +0000

    wip

---
+++ src/AirlinesEnServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Squire;
+
+use Illuminate\Support\ServiceProvider;
+use Squire\Models\Airline;
+
+class AirlinesEnServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        Repository::registerSource(Airline::class, 'en', __DIR__.'/../resources/data.csv');
+    }
+}
